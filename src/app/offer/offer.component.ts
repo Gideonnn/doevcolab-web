50d9e8a88902daf56f17ace774d7d044f79d7a76
Gideon Spierings <gideon.spierings@devolksbank.nl>
2018-07-11 19:11:27 +0200

    Added OfferService and added plumbing to inject it into OfferComponent

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+// Models
+import { Offer } from './models';
+
+// Services
+import { OfferService } from './services';
 
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.scss']
 })
-export class OfferComponent {
+export class OfferComponent implements OnInit {
 
   form: FormGroup;
+  offers: Offer[];
 
-  constructor() {
+  constructor(private offerService: OfferService) {
     this.form = new FormGroup({
       customer: new FormControl('', Validators.required),
       buildingNumber: new FormControl('', Validators.required),
@@ -20,5 +27,13 @@ export class OfferComponent {
       email: new FormControl('', Validators.required),
       project: new FormControl('', Validators.required),
     });
+  }
+
+  ngOnInit() {
+    this.offerService
+      .getAll()
+      .subscribe(offers => {
+        this.offers = offers;
+      });
   }
 }
---
+++ src/app/offer/services/offer.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+// Models
+import { ApiOffer, Offer } from '../models';
+
+// Utils
+import { toOffer } from '../utils/offer.utils';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfferService {
+
+  readonly base = 'http://localhost:3000/offertes';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Offer[]> {
+    return this.http
+      .get<ApiOffer[]>(this.base)
+      .pipe(
+        map(offers => offers.map(offer => toOffer(offer))),
+        catchError((error: HttpErrorResponse) => throwError(error)),
+      );
+  }
+}
