359d993f2bce55010bb43d13b1d2bf0ced066988
Gideon Spierings <gideon.spierings@devolksbank.nl>
2018-07-18 15:34:53 +0200

    Added a api model for Product

---
+++ src/app/offer/models/api-product.model.ts
@@ -0,0 +1,10 @@
+export interface ApiProduct {
+  articleNumber: string;
+  brand: string;
+  _id: string;
+  model: string;
+  properties: string[];
+  purchasePrice: number;
+  sellingPrice: number;
+  supplier: string;
+}
--- src/app/offer/models/index.ts
+++ src/app/offer/models/index.ts
@@ -1,5 +1,6 @@
 export * from './api-offer.model';
 export * from './api-product-group.model';
+export * from './api-product.model';
 export * from './offer.model';
 export * from './product-group.model';
 export * from './product.model';
